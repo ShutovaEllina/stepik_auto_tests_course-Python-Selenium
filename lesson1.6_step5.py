5e3c7913d534262df5cc11d9e51d45a862f9300b
Ellina Shutova <ellina0411@mail.ru>
2023-01-17 10:25:33 +0300

    homework for lessons 1.6.4 and 1.6.5

---
+++ lesson1.6_step5.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time 
+import math
+
+link = "http://suninjuly.github.io/find_link_text"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    link = browser.find_element(By.PARTIAL_LINK_TEXT, str(math.ceil(math.pow(math.pi, math.e)*10000)))
+    link.click()
+    
+    input1 = browser.find_element(By.TAG_NAME, "input")
+    print(input1)
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, "last_name")
+    print(input2)
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.CLASS_NAME, "form-control.city")
+    print(input3)
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element(By.ID, "country")
+    print(input4)
+    input4.send_keys("Russia")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+
+finally:
+    time.sleep(7)
+    browser.quit()
+
